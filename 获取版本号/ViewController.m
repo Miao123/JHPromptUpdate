7ae9d039f0779191b3501e1126fdff9329368036
Miao123 <miao_ios@163.comc>
2017-08-03 08:04:58 +0800

    提示更新

--- 获取版本号/ViewController.m
+++ 获取版本号/ViewController.m
@@ -17,7 +17,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *str = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    NSLog(@"%@",str);
+    [self hsUpdateApp];
+
 }
+-(void)hsUpdateApp
+{
+    //2先获取当前工程项目版本号
+    NSDictionary *infoDic=[[NSBundle mainBundle] infoDictionary];
+    NSString *currentVersion=infoDic[@"CFBundleVersion"];
+    
+    //3从网络获取appStore版本号
+    NSError *error;
+    NSData *response = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/lookup?id=1250712007"]]] returningResponse:nil error:nil];https:  //
+    if (response == nil) {
+        NSLog(@"你没有连接网络哦");
+        return;
+    }
+    NSDictionary *appInfoDic = [NSJSONSerialization JSONObjectWithData:response options:NSJSONReadingMutableLeaves error:&error];
+    if (error) {
+        NSLog(@"hsUpdateAppError:%@",error);
+        return;
+    }
+    NSArray *array = appInfoDic[@"results"];
+    NSDictionary *dic = array.lastObject;
+    NSString *appStoreVersion = dic[@"version"];
+    //打印版本号
+    NSLog(@"当前版本号:%@\n商店版本号:%@",currentVersion,appStoreVersion);
+    //4当前版本号小于商店版本号,就更新
+    if([currentVersion floatValue] < [appStoreVersion floatValue]){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"版本有更新" message:[NSString stringWithFormat:@"检测到新版本(%@),是否更新?",appStoreVersion] delegate:self cancelButtonTitle:@"取消"otherButtonTitles:@"更新",nil];
+        [alert show];
+    }else{
+        NSLog(@"版本号好像比商店大噢!检测到不需要更新");
+    }
+}
+
+- (void)alertView:(UIAlertView*)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    //5实现跳转到应用商店进行更新
+    if(buttonIndex==1){
+        //6此处加入应用在app store的地址，方便用户去更新，一种实现方式如下：
+        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/gb/app/com.ganinfo.softwarepark/id1250712007?mt=8"]];
+        [[UIApplication sharedApplication] openURL:url];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
